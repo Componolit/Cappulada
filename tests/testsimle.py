dff0b874700312a47650d1f52c7fcf0844034a89
Johannes Kliemann <kliemann@componolit.com>
2018-08-06 15:59:23 +0200

    unittest frame

---
+++ tests/testsimle.py
@@ -0,0 +1,11 @@
+
+import unittest
+from capdpa import convert_header
+
+class SimpleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        print("test")
