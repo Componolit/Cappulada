abd0de783ac1d25361ab049aeb4c95e976ba2f4a
Alexander Senier <senier@componolit.com>
2018-08-23 10:44:55 +0200

    Use Class implementation for Namespace

--- capdpa/CXX/cxx_class.py
+++ capdpa/CXX/cxx_class.py
@@ -1,19 +1,23 @@
 import cxx
-import cxx_unit
 
 class NotImplemented(Exception):
 
     def __init__(self, message):
         super(NotImplemented, self).__init__(message);
 
-class Class(cxx_unit.Unit):
+class Class(cxx.Base):
 
     def __init__(self, name, constructor=None, members=None, functions=None, constants=None, enums=None):
 
+        super(Class, self).__init__()
+        self.name        = name
         self.constructor = constructor
         self.members     = members or []
         self.functions   = functions or []
+        self.constants   = constants or []
+        self.enums       = enums or []
 
+    def UsedPackages(self):
         types = []
         for f in self.functions:
             for p in f.parameters:
@@ -27,42 +31,71 @@ class Class(cxx_unit.Unit):
         used_packages = set()
         for t in types:
             # Local varialbe or fully quallified local type, no package needed
-            if not t.name.PackagePath() or t.name.PackagePath() == name.PackageFull():
+            if not t.name.PackagePath() or t.name.PackagePath() == self.name.PackageFull():
                 continue
             used_packages.add(t.name.PackagePathName())
-
-        super(Class, self).__init__(name, constants, enums, sorted(list(used_packages)))
+        return sorted(list(used_packages))
 
     def AdaSpecification(self):
 
-        # Generate record members
-        if self.members:
-            class_record = "record\n"
-            for member in self.members:
-                class_record += "      " + member.AdaSpecification() + ";\n"
-            class_record += "   end record"
-        else:
-            class_record = "null record"
+        # Generate with statements
+        withs = ""
+        for w in self.UsedPackages():
+            withs += "with " + w + ";\n"
+        if withs:
+            withs += "\n"
 
-        # generate primitive operations for tagged type
-        ops=""
-        if self.functions:
-            for f in self.functions:
-                ops += "   " + f.AdaSpecification()
+        class_record = ""
+        constructor = ""
+        ops = ""
+
+        if self.constructor:
+            # Generate record members
+            class_record = '   type %(type)s is\n   tagged ' \
+                % { 'type': self.name.PackageBaseName() }
+            if self.members:
+                class_record += "record\n"
+                for member in self.members:
+                    class_record += "      " + member.AdaSpecification() + ";\n"
+                class_record += "   end record;\n"
+            else:
+                class_record += "null record;\n"
+
+            # generate constructor
+            constructor = \
+                ('   function Constructor return %(type)s\n' +
+                 '   with Import => "%(symbol)s";\n') \
+                    % { 'type':   self.name.PackageBaseName(),
+                        'symbol': self.constructor.symbol }
+
+            # generate primitive operations for tagged type
+            ops=""
+            if self.functions:
+                for f in self.functions:
+                    ops += "   " + f.AdaSpecification()
+
+        enums = ""
+
+        constants = ""
+        for c in self.constants:
+            constants += "   " + c.AdaSpecification() + "\n"
 
         # Main package structure
         p = \
-            '%(header)s'                                + \
-            '   type %(type)s is\n'                     + \
-            '   tagged %(record)s;\n'                   + \
-            '   function Constructor return %(type)s\n' + \
-            '   with Import => "%(symbol)s";\n'         + \
+            '%(withs)s'                                 + \
+            'package %(package)s\n'                     + \
+            'is\n'                                      + \
+            '%(constants)s'                             + \
+            '%(enums)s'                                 + \
+            '%(record)s'                                + \
+            '%(constructor)s'                           + \
             '%(ops)s'                                   + \
-            '%(footer)s'
+            'end %(package)s;\n'
 
-        return p % { 'header':  self.SerializeHeader(),
-                     'type':    self.name.PackageBaseName(),
-                     'record':  class_record,
-                     'ops':     ops,
-                     'symbol':  self.constructor.symbol,
-                     'footer':  self.SerializeFooter() }
+        return p % { 'withs':       withs,
+                     'package':     self.name.PackageFullName(),
+                     'constants':   constants,
+                     'enums':       enums,
+                     'record':      class_record,
+                     'constructor': constructor,
+                     'ops':         ops }
--- capdpa/CXX/cxx_namespace.py
+++ capdpa/CXX/cxx_namespace.py
@@ -1,8 +1,11 @@
 import cxx
+import cxx_class
 
-class Namespace(cxx.Base):
+class Namespace(cxx_class.Class):
 
     def __init__(self, name, namespaces=None, classes=None, constants=None, enums=None):
-        super(Namespace, self).__init__()
         self.namespaces = namespaces or []
         self.classes = classes or []
+        super(Namespace, self).__init__(name      = name,
+                                        constants = constants,
+                                        enums     = enums)
