fe8c965fa0b2806e4f808c3805e62462ebab659c
Johannes Kliemann <kliemann@componolit.com>
2018-10-29 14:42:37 +0100

    fix virtual inheriting from non-virtual

--- capdpa/IR/ir_class.py
+++ capdpa/IR/ir_class.py
@@ -67,10 +67,12 @@ class Class(ir.Base):
         # Generate record members
         null = type("", (), dict(AdaSpecification=lambda self, indentation: " " * indentation + "null"))()
         base = [Class_Reference.isInst(c) for c in self.children]
-        base = self.children[base.index(True)].name.PackageFullName() if True in base else ""
+        base = self.children[base.index(True)] if True in base else None
+        hasvirtualbase = base.isVirtual() if base else False
 
-        class_record = '   type Class is{}\n'.format((" new " + base + " with") if base and self.isVirtual() else "")
-        class_record += '   {}limited '.format("tagged " if self.isVirtual() and not base else "")
+        class_record = '   type Class is{}\n'.format(
+                (" new " + base.name.PackageFullName() + " with") if hasvirtualbase and self.isVirtual() else "")
+        class_record += '   {}limited '.format("tagged " if self.isVirtual() and not hasvirtualbase else "")
         class_record += "record\n"
         for member in self.Members() or [null]:
             class_record += member.AdaSpecification(indentation=6) + ";\n"
