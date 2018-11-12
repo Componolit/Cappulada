db7f5c85da1e7c0bf199a49a516c9785511db838
Johannes Kliemann <kliemann@componolit.com>
2018-11-12 18:02:40 +0100

    add cache dir if missing fixes #24

--- tests/capdpa_test.py
+++ tests/capdpa_test.py
@@ -1,11 +1,19 @@
 
+import os
 import unittest
 from capdpa import *
 
 class Capdpa_Test(unittest.TestCase):
 
 
-    check = lambda self, a, b: self.assertEqual(a, b, "Invalid entity: >>>\n" + str(a) + "\n<<< expected: >>>\n" + str(b) + "\n<<<")
-    load = lambda self, spec: open("tests/data/" + spec, "r").read()
-    cache = lambda self, res, spec: open("tests/cache/" + spec, "w").write(res)
+    def check(self, a, b):
+        return self.assertEqual(a, b, "Invalid entity: >>>\n" + str(a) + "\n<<< expected: >>>\n" + str(b) + "\n<<<")
+
+    def load(self, spec):
+        return open("tests/data/" + spec, "r").read()
+
+    def cache(self, res, spec):
+        if not os.path.exists("tests/cache/"):
+            os.mkdir("tests/cache/")
+        return open("tests/cache/" + spec, "w").write(res)
 
