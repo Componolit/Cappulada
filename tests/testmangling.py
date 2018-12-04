2d709f336e750e3ed44a6e1d4892a03f4d363b0e
Alexander Senier <senier@componolit.com>
2018-12-04 15:50:44 +0100

    Very basic mangling support
    Ref. #9

--- tests/testmangling.py
+++ tests/testmangling.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import pprint
 from capdpa import *
 
 class Mangling(unittest.TestCase):
@@ -10,34 +11,49 @@ class Mangling(unittest.TestCase):
                    children = [Function(name = "foo",
                                         symbol     = "",
                                         parameters = None)])
-        result = c.children[1].Mangle()
+        result = c.children[1].Mangle("Capdpa")
         self.assertTrue(result == "_ZN12With_Virtual3fooEv", "Invalid symbol: " + result)
 
     def test_class_with_functions_void (self):
         c = Class (name     = "With_functions",
                   children  = [Function(name = "public_function",
                                         symbol     = "",
-                                        parameters = [Variable (None, Type_Reference(["int"]))])])
+                                        parameters = [Variable (None, Type_Reference(name=Identifier(["Capdpa", "int"])))])])
 
-        result = c.children[1].Mangle()
+        result = c.children[1].Mangle("Capdpa")
         self.assertTrue(result == "_ZN14With_functions15public_functionEi", "Invalid symbol: " + result)
 
     def test_class_with_functions_int (self):
         c = Class (name     = "With_functions",
                    children = [Function(name        = "named_param",
                                         symbol      = "",
-                                        parameters  = [Variable ("param", Type_Reference(["int"]))],
+                                        parameters  = [Variable (Identifier(name=["Capdpa", "param"]), Type_Reference(name=Identifier(["Capdpa", "int"])))],
                                         return_type = Type_Reference(["int"]))])
 
-        result = c.children[1].Mangle()
+        result = c.children[1].Mangle("Capdpa")
         self.assertTrue(result == "_ZN14With_functions11named_paramEi", "Invalid symbol: " + result)
 
     def test_class_with_functions_ctor (self):
         c = Class (name     = "With_functions",
                    children = [Constructor(symbol     = "",
                                            parameters = None)])
-        result = c.children[0].Mangle()
+        result = c.children[0].Mangle("Capdpa")
         self.assertTrue(result == "_ZN14With_functionsC1Ev", "Invalid symbol: " + result)
+
+    def test_template (self):
+        templates = CXX("tests/data/test_template_mangling.h").ToIR(project="Capdpa").children
+
+        functions = templates[2].children
+        symbol = functions[1].Mangle("Capdpa")
+        self.assertTrue (symbol == "_ZN6Capdpa3Cls3barE6Capdpa5TemplIciEc", "Invalid symbol: " + symbol)
+        symbol = functions[2].Mangle("Capdpa")
+        self.assertTrue (symbol == "_ZN6Capdpa3Cls3fooEic", "Invalid symbol: " + symbol)
+        symbol = functions[3].Mangle("Capdpa")
+        self.assertTrue (symbol == "_ZN6Capdpa3Cls3bazE6Capdpa5TemplIciES0_IccE", "Invalid symbol: " + symbol)
+
+        functions = templates[1].children
+        symbol = functions[1].Mangle("Capdpa")
+        self.assertTrue (symbol == "_ZN6Capdpa3Cls3varE6Capdpa3VarIJEEc", "Invalid symbol: " + symbol)
 
     # FIXME: Missing
     #   * Templates
