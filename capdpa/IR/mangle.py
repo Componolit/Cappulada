abf5cf77c007019f5085fc03e32fc9c72472b49f
Alexander Senier <senier@componolit.com>
2018-12-12 15:03:39 +0100

    Restructure namedb queries
    Ref. #9

--- capdpa/IR/mangle.py
+++ capdpa/IR/mangle.py
@@ -6,14 +6,14 @@ class Name:
         self.name = name[1:]
 
         self.entity = entity
-        self.pointer = pointer
+        self.pointer = pointer > 0
         self.constant = constant
         self.reference = reference
 
         # Defined here:
         # https://itanium-cxx-abi.github.io/cxx-abi/abi.html#mangling-builtin
         self.builtins = {
-            'void':                  "v",
+            'void':                  'v',
             'wchar_t':               'w',
             'bool':                  'b',
             'char':                  'c',
@@ -36,6 +36,9 @@ class Name:
             'C_Address':             'v',
         }
 
+    def __repr__ (self):
+        return ".".join(self.name + [self.entity] if self.entity else []) + ": pointer=" + str(self.pointer) + " constant=" + str(self.constant) + " reference=" + str(self.reference)
+
     def is_builtin (self):
 
         return len(self.name) == 0 and self.entity in self.builtins
@@ -47,7 +50,7 @@ class Name:
         # C_Address actually means void* which we mangle as a pointer 'P' and
         # a builtin type 'v'. FIXME: Wouldn't it be better to actually set
         # self.pointer and a 'void' type in capdpa/cxx.py?
-        result += "P" if self.pointer > 0 or self.entity == "C_Address" else ""
+        result += "P" if self.pointer or self.entity == "C_Address" else ""
         result += "K" if self.constant else ""
         result += "R" if self.reference else ""
 
@@ -67,54 +70,51 @@ class Namedb:
         result = ""
 
         if name.is_builtin():
-            return (name.substitution(), [])
+            return (name.substitution(), False)
 
         # Find longest match in database
         last = len(name.name)
-        while not name.name[0:last] in self.db and last > 0:
+        while last > 0 and not (name.name[0:last], name.pointer, name.constant, name.reference) in self.db:
             last -= 1
 
         # Found a match
         if last > 0:
-            index = self.db.index (name.name[0:last])
+            index = self.db.index ((name.name[0:last], name.pointer, name.constant, name.reference))
             tag   = str(index - 1) if index > 0 else ""
             result += "S" + tag + "_"
 
         # Add new names to db
         for i in range(1, len(name.name) + 1):
-            if not name.name[0:i] in self.db:
-                self.db.append (name.name[0:i])
+            new = (name.name[0:i], name.pointer, name.constant, name.reference)
+            if not new in self.db:
+                self.db.append (new)
 
-        return (result, name.name[last:])
-
-    def Get (self, name, entity, pointer=0, constant=False, reference=False):
-
-        result = ""
-
-        n = Name (name, entity, pointer, constant, reference)
-
-        # Query name in database
-        (symbol, remainder) = self.Query (n)
-        result += symbol
+        if last == 0:
+            const = (name.name, False, True, False)
+            if name.constant and not const in self.db:
+                self.db.append (const)
 
         # Handle all other parts as normal
-        for i in remainder:
+        for i in name.name[last:]:
             result += str(len(i)) + i
 
-        if not entity or entity == "Class" or n.is_builtin():
+        if not name.entity or name.entity == "Class" or name.is_builtin():
             pass
-        elif entity == "__constructor__":
+        elif name.entity == "__constructor__":
             result += "C1"
         else:
-            result += str(len(entity)) + entity
-
-        # All elements of name have been replaced by reference
-        fully_compressed = not remainder
+            result += str(len(name.entity)) + name.entity
 
         # At least one component and not fully compressed name
-        nested = (symbol or entity) and not fully_compressed
+        nested = (len(name.name) > 1 or name.entity) and last != len(name.name)
+        return (result, nested)
+
+    def Get (self, name, entity, pointer=False, constant=False, reference=False):
+
+        # Query name in database
+        n = Name (name, entity, pointer, constant, reference)
+        (result, nested) = self.Query (n)
 
         if nested:
             result = "N" + result + "E"
-
-        return "{prefix}{result}".format(prefix = n.prefix(), result  = result)
+        return n.prefix() + result
