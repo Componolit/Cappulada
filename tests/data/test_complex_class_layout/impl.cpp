70a9b9173825d2ef164b45bfa8ef7a1be955bbb3
Alexander Senier <senier@componolit.com>
2018-12-27 14:01:51 +0100

    Test complex class layout
    Ref. #52

---
+++ tests/data/test_complex_class_layout/impl.cpp
@@ -0,0 +1,15 @@
+class Cls {
+   public:
+      char val1;
+      int val2;
+      float val3;
+      char val4;
+      unsigned long long val5;
+      char val6;
+
+      Cls();
+      Cls(char val1, int val2, float val3, char val4, unsigned long long val5, char val6);
+};
+
+Cls::Cls(char val1, int val2, float val3, char val4, unsigned long long val5, char val6) :
+   val1(val1), val2(val2), val3(val3), val4(val4), val5(val5), val6(val6) { };
