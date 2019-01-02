c45354e8a7bc14f5b37e4a11e4d6ac842f83fa6e
Alexander Senier <senier@componolit.com>
2019-01-02 18:14:13 +0100

    Test class members
    Ref. #52

---
+++ tests/data/test_class_members/impl.h
@@ -0,0 +1,10 @@
+class Cls {
+   public:
+      Cls();
+      Cls(int, float, bool, unsigned long, short);
+      int int_member;
+      float float_member;
+      bool bool_member;
+      unsigned long ul_member;
+      short short_member;
+};
