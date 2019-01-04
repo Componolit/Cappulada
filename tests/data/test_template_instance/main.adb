3efbd33afd6322633235431a60eaa6e2a2f02b75
Alexander Senier <senier@componolit.com>
2019-01-04 14:38:52 +0100

    Test template instantiation
    Ref. #52

--- tests/data/test_template_instance/main.adb
+++ tests/data/test_template_instance/main.adb
@@ -1,13 +1,21 @@
 with Tests;
-with Interfaces.C.Extensions;
+with Interfaces.C;
 with Test_Template_Instance.Cls_T_Int;
 with Test_Template_Instance.Cls_T_Char;
-with Test_Template_Instance.Foo;
 
 procedure Main
 is
+   use Interfaces.C;
+   Char_Val : aliased char;
+   Int_Val  : aliased int;
+
    use Test_Template_Instance;
+   Char_Inst : aliased Cls_T_Char.Class := Cls_T_Char.Constructor ('X');
+   Int_Inst  : aliased Cls_T_Int.Class  := Cls_T_Int.Constructor (12365345);
 begin
-   --  Tests.Assert (Ld   = 593430024.559,            "Wrong value returned (17): " & Ld'Img);
-   null;
+   Cls_T_Char.Get (Char_Inst'Access, Char_Val'Access);
+   Cls_T_Int.Get (Int_Inst'Access, Int_Val'Access);
+
+   Tests.Assert (Char_Val = 'X', "Wrong value returned (1): " & Char_Val'Img);
+   Tests.Assert (Int_Val = 12365345, "Wrong value returned (2): " & Int_Val'Img);
 end Main;
