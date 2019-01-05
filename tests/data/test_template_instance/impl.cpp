4b477388ee3cafca3fc2e507a14093d7033bc4b5
Alexander Senier <senier@componolit.com>
2019-01-05 21:13:10 +0100

    Rename test case parameters

--- tests/data/test_template_instance/impl.cpp
+++ tests/data/test_template_instance/impl.cpp
@@ -4,10 +4,10 @@ template<typename T>
 Cls<T>::Cls() { };
 
 template<typename T>
-Cls<T>::Cls(T val) : _data(val) { };
+Cls<T>::Cls(T val1) : _data(val1) { };
 
 template<typename T>
-void Cls<T>::get(T &val) { val = _data; };
+void Cls<T>::get(T &val2) { val2 = _data; };
 
 void Unused(char &c, int &i) {
    Foo::cls_inst1.get(c);
