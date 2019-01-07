535a24f349d804ad17923bfedeb814e6e21c6fa6
Alexander Senier <senier@componolit.com>
2019-01-07 08:48:32 +0100

    Test references returned from class template method
    Ref. #52

--- tests/data/test_template_instance/impl.cpp
+++ tests/data/test_template_instance/impl.cpp
@@ -9,7 +9,13 @@ Cls<T>::Cls(T val1) : _data(val1) { };
 template<typename T>
 void Cls<T>::get(T &val2) { val2 = _data; };
 
+template<typename T>
+T& Cls<T>::get() { return _data; };
+
 void Unused(char &c, int &i) {
    Foo::cls_inst1.get(c);
+   c = Foo::cls_inst1.get();
+
    Foo::cls_inst2.get(i);
+   i = Foo::cls_inst2.get();
 }
--- tests/data/test_template_instance/main.adb
+++ tests/data/test_template_instance/main.adb
@@ -8,14 +8,22 @@ is
    use Interfaces.C;
    Char_Val : aliased char;
    Int_Val  : aliased int;
+   Char_Ref : access char;
+   Int_Ref  : access int;
 
    use Test_Template_Instance;
    Char_Inst : aliased Cls_T_Char.Class := Cls_T_Char.Constructor ('X');
    Int_Inst  : aliased Cls_T_Int.Class  := Cls_T_Int.Constructor (12365345);
 begin
    Cls_T_Char.Get (Char_Inst'Access, Char_Val'Access);
+   Char_Ref := Cls_T_Char.Get (Char_Inst'Access);
+
    Cls_T_Int.Get (Int_Inst'Access, Int_Val'Access);
+   Int_Ref := Cls_T_Int.Get (Int_Inst'Access);
 
    Tests.Assert (Char_Val = 'X', "Wrong value returned (1): " & Char_Val'Img);
-   Tests.Assert (Int_Val = 12365345, "Wrong value returned (2): " & Int_Val'Img);
+   Tests.Assert (Char_Ref.all = 'X', "Wrong value returned (2): " & Char_Ref.all'Img);
+
+   Tests.Assert (Int_Val = 12365345, "Wrong value returned (3): " & Int_Val'Img);
+   Tests.Assert (Int_Ref.all = 12365345, "Wrong value returned (4): " & Int_Ref.all'Img);
 end Main;
