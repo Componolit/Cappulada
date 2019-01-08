57d827ae793c2a31ed27372f4de4087572b9b37c
Alexander Senier <senier@componolit.com>
2019-01-08 23:17:40 +0100

    Print delta if string compare fails

--- tests/capdpa_test.py
+++ tests/capdpa_test.py
@@ -1,13 +1,14 @@
 
 import os
 import unittest
+import difflib
 from capdpa import *
 
 class Capdpa_Test(unittest.TestCase):
 
-
     def check(self, a, b):
-        return self.assertEqual(a, b, "Invalid entity: >>>\n" + str(a) + "\n<<< expected: >>>\n" + str(b) + "\n<<<")
+        return self.assertEqual(a, b,
+            "Invalid entity:\n " + " ".join(difflib.unified_diff(a.splitlines(1), b.splitlines(1), "Expected", "Result")))
 
     def load(self, spec):
         return open("tests/data/" + spec, "r").read()
