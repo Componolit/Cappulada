45ce7115431e849ca88b9d4ab9a6cd351738d1e2
Alexander Senier <senier@componolit.com>
2019-01-14 22:24:03 +0100

    Changes due to #63

--- tests/data/test_multiple_members_with_private_type/main.adb
+++ tests/data/test_multiple_members_with_private_type/main.adb
@@ -6,5 +6,7 @@ procedure Main
    with SPARK_Mode => Off
 is
 begin
+   --  No actual test, we only want to ensure that the same private type
+   --  declaration is generated only once.
    null;
 end Main;
