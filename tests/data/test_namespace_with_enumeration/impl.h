8cf13a92103637388531a73d2814fead318e037b
Johannes Kliemann <kliemann@componolit.com>
2019-02-07 14:57:13 +0100

    fix missing default constructors

--- tests/data/test_namespace_with_enumeration/impl.h
+++ tests/data/test_namespace_with_enumeration/impl.h
@@ -2,4 +2,5 @@ class Cls {
    public:
       enum Tage { Montag, Dienstag, Mittwoch, Donnerstag, Freitag, Sonnabend, Sonntag };
       enum Numbered_Days { Monday = 17, Tuesday = 23, Wednesday = 111, Thursday = 5, Friday = 10, Saturday = 1, Sunday = 654 };
+      Cls();
 };
--- tests/data/test_template_instance/impl.cpp
+++ tests/data/test_template_instance/impl.cpp
@@ -12,10 +12,12 @@ void Cls<T>::get(T &val2) { val2 = _data; };
 template<typename T>
 T& Cls<T>::get() { return _data; };
 
-void Unused(char &c, int &i) {
+void Unused(char &c, int &i, Cls<char> &cc, Cls<int> &ci) {
+   cc = Cls<char>();
    Foo::cls_inst1.get(c);
    c = Foo::cls_inst1.get();
 
+   ci = Cls<int>();
    Foo::cls_inst2.get(i);
    i = Foo::cls_inst2.get();
 }
