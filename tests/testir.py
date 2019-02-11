d30c2365aa236ef277c30d031c8065d0851f56cf
Johannes Kliemann <kliemann@componolit.com>
2019-02-11 16:52:41 +0100

    add proper contains check ref #83

--- tests/testir.py
+++ tests/testir.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+if __name__ == '__main__':
+    sys.path.append (".")
+
 import unittest
 from capdpa import *
 from capdpa_test import *
@@ -51,6 +55,32 @@ class IR_Test(Capdpa_Test):
             Class(name="C", children=[
                 Enum(name="E", children=[const])])])
         self.check(const.FullyQualifiedName(), ["N", "C", "E", "K"])
+
+    def test_contains(self):
+        const = Constant(name="K", value=1)
+        tree = Namespace(name="N", children=[
+            Class(name="C", children=[
+                Enum(name="E", children=[const])])])
+        self.check("K" in tree, False)
+        self.check("C" in tree, True)
+
+    def test_contains_list(self):
+        const = Constant(name="K", value=1)
+        tree = Namespace(name="N", children=[
+            Class(name="C", children=[
+                Enum(name="E", children=[const])])])
+        self.check(["C", "E", "K"] in tree, True)
+        self.check(["C"] in tree, True)
+        self.check(["C", "F"] in tree, False)
+
+    def test_contains_tuple(self):
+        const = Constant(name="K", value=1)
+        tree = Namespace(name="N", children=[
+            Class(name="C", children=[
+                Enum(name="E", children=[const])])])
+        self.check(("C", "E", "K") in tree, True)
+        self.check(("C") in tree, True)
+        self.check(("C", "F") in tree, False)
 
 if __name__ == '__main__':
     unittest.main()
