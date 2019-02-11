c13582684adaa27779e06bd01afcd3cf7fe518c9
Johannes Kliemann <kliemann@componolit.com>
2019-02-11 16:52:09 +0100

    remove array template ref #83

--- capdpa/IR/ir_array.py
+++ capdpa/IR/ir_array.py
@@ -16,7 +16,3 @@ class Array(ir.Base):
     def AdaSpecification(self, indentation=0, private=False):
         return self.ctype.AdaSpecification(indentation, private) + "_Array (1 .. {})".format(self.size)
 
-class Array_Template(Array, ir_template.Template_Reference):
-
-    def __init__(self, ctype, argument):
-        super(Array_Template, self).__init__(ctype=ctype, size=0)
