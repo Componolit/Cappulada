f43e2f0644fc6871e51de5f37d08fe020537cda2
Johannes Kliemann <kliemann@componolit.com>
2019-02-11 14:42:55 +0100

    implement TYPE_REF ref #32

---
+++ tests/data/convert/test_type_ref.h
@@ -0,0 +1,19 @@
+
+struct Tr
+{ };
+
+template <typename T1, typename T2, typename T3, int T4, typename T5, char T6, bool T7>
+struct Tt
+{ };
+
+template <>
+struct Tt<Tr, int, Tr, 4, Tr, 't', true>
+{ };
+
+class T
+{
+    public:
+        Tt<Tr, int, Tr, 4, Tr, 't', true> t;
+};
+
+//void func(Tt<Tr, int, Tr, 4, Tr, 't', true>);
